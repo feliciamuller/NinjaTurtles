d0bab13c388cdc6b02ae4441120b3a742e6882e5
Felicia Müller <felicia-muller@hotmail.com>
2024-03-03 12:07:21 +0100

    first commit

---
+++ OneDrive/Skrivbord/react-work-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import NotionDataReader from './components/NotionDataReader';
+
+function App() {
+  return (
+    <div className = 'App'>
+      <NotionDataReader/>
+    </div>
+  );
+}
+
+export default App;
