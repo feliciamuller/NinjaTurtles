492693d0a3cc2fe241319b30631df8820a86a7ac
Felicia Müller <felicia-muller@hotmail.com>
2024-03-07 14:50:10 +0100

    added login logic and layout

--- OneDrive/Skrivbord/react-work-app/src/App.js
+++ OneDrive/Skrivbord/react-work-app/src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-import NotionDataReader from './components/NotionDataReader';
+import Home from './components/Home';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import Login from './components/Login';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [email, setEmail] = useState('')
+
   return (
     <div className = 'App'>
-      <NotionDataReader/>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />} />
+        <Route path="/Login" element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
+      </Routes>
+        </BrowserRouter>
     </div>
   );
 }
---
+++ OneDrive/Skrivbord/react-work-app/src/components/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Login from './Login';
+
+const Home = (props) => {
+  const { loggedIn, email } = props
+  const navigate = useNavigate()
+
+  const onButtonClick = () => {
+    navigate("/Login")
+  }
+
+  return (
+    <div className="mainContainer">
+      <div className={'titleContainer'}>
+        <div>Welcome!</div>
+      </div>
+      <div>This is the home page.</div>
+      <div className={'buttonContainer'}>
+        <input
+          className={'inputButton'}
+          type="button"
+          onClick={onButtonClick}
+          value={loggedIn ? 'Log out' : 'Log in'}
+        />
+        {loggedIn ? <div>Your email address is {email}</div> : <div />}
+      </div>
+    </div>
+  )
+}
+
+export default Home;
