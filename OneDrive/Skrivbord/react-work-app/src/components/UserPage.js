a27a97b86d6f4378995eb60a69b2703679020464
fredrik nellbeck <Foggel@live.se>
2024-03-16 10:34:31 +0100

    Update Login logic

--- OneDrive/Skrivbord/react-work-app/src/components/UserPage.js
+++ OneDrive/Skrivbord/react-work-app/src/components/UserPage.js
@@ -2,8 +2,16 @@ import React, {useState} from "react";
 import axios from "axios";
 
 const UserPage = () => {
-    const userName = localStorage.getItem("userName");
-    return userName
+    const [userName, setUserName] = useState("")
+
+    const onButtonClick = () => {
+        setUserName(localStorage.getItem("userName"))
+    }
+    return (
+        <>
+                <button onClick={onButtonClick}></button>
+        </>
+    )
 }
 
 export default UserPage
